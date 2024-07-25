ff403876c5696c3be0b623cc908702e35065a67d
juliana1390 <juliana13290@gmail.com>
2024-07-25 16:12:43 -0300

    login/cadastro/recuperacao: background color

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -35,6 +35,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
+    backgroundColor: '#ffffff',
   },
   text: {
     fontSize: 18,
--- src/pages/RecoverPassword/index.js
+++ src/pages/RecoverPassword/index.js
@@ -1,22 +1,61 @@
 import React from "react";
-import  { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import Logo from '../../components/Logo';
+import EmailInput from '../../components/EmailInput';
 
-export default function RedefinePassword({ navigation }) {
+export default function Login({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Página RedefinePassword</Text>
+      <Logo />
+      <Text style={styles.text}>Insira o e-mail para recuperar a senha</Text>
+      <EmailInput />
+      <View style={styles.flexGrow} />
+      <Button 
+        title="Enviar e-mail de recuperação"
+        onPress={() => navigation.navigate('HomeScreen')}
+        style={styles.sendButton}
+      />
+      <Text style={styles.linkText}
+          onPress={() => navigation.navigate('LoginScreen')}>
+        Fazer login
+      </Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
   text: {
-    fontSize: 25,
-    fontWeight: 'bold'
-  }
-})
+    fontSize: 18,
+    fontWeight: 'regular',
+    marginVertical: 10,
+    color: '#444444',
+    textAlign: 'center',
+  },
+  linkText: {
+    fontSize: 16,
+    fontWeight: 'regular',
+    textDecorationLine: 'underline',
+    color: 'blue',
+    marginTop: 12,
+  },
+  flexGrow: {
+    flexGrow: 2,
+  },
+  sendButton: {
+    marginBottom: 20,
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 20,
+    color: '#0168BC',
+    borderColor: '#0168BC',
+  },
+});
