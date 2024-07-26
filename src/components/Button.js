385c0025116981b7e43356a346eba0faf2451aa0
juliana1390 <juliana13290@gmail.com>
2024-07-26 10:52:07 -0300

    botão flex

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
 export default function Button({ handlePress, buttonText }) {
   return (
-    <TouchableOpacity onPress={handlePress} >
+    <TouchableOpacity onPress={handlePress} style={styles.container}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{buttonText}</Text>
       </View>
@@ -12,17 +12,23 @@ export default function Button({ handlePress, buttonText }) {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 20,
+  },
   button: {
     height: 40,
-    width: 300,
     backgroundColor: '#0168BC',
+    borderWidth: 1,
     borderRadius: 20,
+    borderColor: '#0168BC',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 20,
+    flexDirection: 'row',
   },
   buttonText: {
-    fontSize: '20',
+    fontSize: 18,
     fontWeight: 'bold',
     color: '#fff',
   }
